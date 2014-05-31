617f605de776bda97e08f6734a1010cb18b0c8ac
sujie <shisujieit@gmail.com>
2014-05-31 22:14:32 +0800

    正则表达式

---
+++ RegExpLib/RegExp.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace RegExpLib
+{
+    public class RegExp
+    {
+        public static bool IsEmail(string match)
+        {
+            return Regex.IsMatch(match, @"[\w!#$%&'*+/=?^_`{|}~-]+(?:\.[\w!#$%&'*+/=?^_`{|}~-]+)*@(?:[\w](?:[\w-]*[\w])?\.)+[\w](?:[\w-]*[\w])?");
+        }
+
+    }
+}
---
+++ RegExpTest/RegExpUnitTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RegExpLib;
+
+namespace RegExpTest
+{
+    [TestClass]
+    public class RegExpUnitTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            string mail = "11@qq.com";
+            bool result = RegExp.IsEmail(mail);
+            Assert.IsTrue(result);
+        }
+    }
+}
