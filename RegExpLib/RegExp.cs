ee51ba7446592a8bd7647eebee13031066a27bdb
Jess <shisujieIT@gmail.com>
2016-09-08 20:24:41 +0800

    整理提交

--- RegExpLib/RegExp.cs
+++ RegExpLib/RegExp.cs
@@ -7,11 +7,19 @@ using System.Threading.Tasks;
 
 namespace RegExpLib
 {
+    /// <summary>
+    /// 正则表达式验证
+    /// </summary>
     public class RegExp
     {
-        public static bool IsEmail(string match)
+        /// <summary>
+        /// 是否是Email地址
+        /// </summary>
+        /// <param name="verify">待验证的字符串</param>
+        /// <returns>验证通过与否</returns>
+        public static bool IsEmail(string verify)
         {
-            return Regex.IsMatch(match, @"[\w!#$%&'*+/=?^_`{|}~-]+(?:\.[\w!#$%&'*+/=?^_`{|}~-]+)*@(?:[\w](?:[\w-]*[\w])?\.)+[\w](?:[\w-]*[\w])?");
+            return Regex.IsMatch(verify, @"[\w!#$%&'*+/=?^_`{|}~-]+(?:\.[\w!#$%&'*+/=?^_`{|}~-]+)*@(?:[\w](?:[\w-]*[\w])?\.)+[\w](?:[\w-]*[\w])?");
         }
 
     }
