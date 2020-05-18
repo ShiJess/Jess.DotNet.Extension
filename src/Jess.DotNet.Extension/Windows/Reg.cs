f16d627531c8af0d8b0ca959b968bc9accfc4300
Jess Smith <shisujie@outlook.com>
2020-05-18 19:21:17 +0800

    add md5 process to roadmap;

---
+++ src/Jess.DotNet.Extension/Windows/Reg.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Jess.DotNet.Extension.Windows
+{
+    /// <summary>
+    /// 注册表相关信息
+    /// </summary>
+    public class Reg
+    {
+    }
+
+    /// <summary>
+    /// 注册表单元
+    /// </summary>
+    public enum RegistryHive : uint
+    {
+        HKEY_CLASSES_ROOT = 0x80000000,
+        HKEY_CURRENT_USER = 0x80000001,
+        HKEY_LOCAL_MACHINE = 0x80000002,
+        HKEY_USERS = 0x80000003,
+        HKEY_CURRENT_CONFIG = 0x80000005,
+        HKEY_DYN_DATA = 0x80000006,
+    }
+
+    /// <summary>
+    /// 注册表项数据类型
+    /// </summary>
+    public enum RegDataType : int
+    {
+        REG_SZ = 1,
+        REG_EXPAND_SZ = 2,
+        REG_BINARY = 3,
+        REG_DWORD = 4,
+        REG_MULTI_SZ = 7,
+        REG_QWORD = 11,
+    }
+
+}
---
+++ src/Jess.FileValidation.MD5/MD5Helper.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Jess.FileValidation.MD5
+{
+    public sealed class MD5Helper
+    {
+        /// <summary>
+        /// 生成MD5文件信息
+        /// 将指定目录下所有文件的md5信息生成为一个文件
+        /// </summary>
+        /// <param name="rootdir"></param>
+        public static void GenerateMD5Info(string rootdir)
+        {
+
+        }
+
+        //public string void GenerateMD5Info
+
+        /// <summary>
+        /// 获取文件中的md5键值对信息
+        /// </summary>
+        /// <param name="md5infopath"></param>
+        /// <returns></returns>
+        public static Dictionary<string, string> GetMD5InfoList(string md5infopath)
+        {
+            return new Dictionary<string, string>();
+        }
+
+        /// <summary>
+        /// 验证md5
+        /// </summary>
+        /// <param name="fileext">需要验证的文件后缀</param>
+        /// <returns></returns>
+        public static bool Validate(string fileext)
+        {
+            return true;
+        }
+    }
+}
